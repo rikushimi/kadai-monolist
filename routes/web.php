d58ac18f803300dd5ac70e50653e1d0f818a778f
riku.shimizu <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-06-20 01:51:52 +0000

    registration

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,6 @@
 <?php
 
  Route::get('/', 'WelcomeController@index');
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
