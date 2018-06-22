bd02c0a556ce3b7268625201fb827e3f256afab7
riku.shimizu <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-06-22 04:57:32 +0000

    listrank

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,4 @@ Route::get('/', 'WelcomeController@index');
 });
 
  Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
-
- Route::group(['middleware' => ['auth']], function () {
-    Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
-    Route::post('want', 'ItemUserController@want')->name('item_user.want');
-    Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
-    Route::resource('users', 'UsersController', ['only' => ['show']]);
-});
+ Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
