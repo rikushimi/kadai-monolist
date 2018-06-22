2926b2163998697db75d8f155c71f573fe6efb4b
riku.shimizu <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-06-22 05:22:02 +0000

    finashimi

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -21,10 +21,15 @@
                             </div>
                         </div>
                         @if (isset($item->count))
-                            <div class="panel-footer">
+                           @if ($item->type = 'want')
+                              <div class="panel-footer">
                                 <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                              </div>    
+                           @else
+                              <div class="panel-footer">    
                                 <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>
-                            </div>
+                              </div>
+                           @endif
                         @endif
                     </div>
                 </div>
